31f2136b700805f797fb91aea658718a9a9c3c7b
Hugh O'Reilly <horeilly1101@gmail.com>
2019-10-27 21:54:59 -0500

    fixed a broken test case

--- tests/test_email/test_email_factory.py
+++ tests/test_email/test_email_factory.py
@@ -8,7 +8,7 @@ class TestGmailClient(unittest.TestCase):
     """Unit test suite for the GmailClient."""
     def test_jinja_email_factory(self):
         fac = JinjaEmailFactory(path.dirname(path.abspath(__file__)), "HR Team")
-        email = fac.create_email("Hello", "test_template.html", "Hacker", "")
+        email = fac.create_email("Hello", "test_template.html", "", first_name="Hacker")
 
         self.assertEqual("HR Team", email.from_name)
         self.assertEqual("Hello", email.email_subject)
