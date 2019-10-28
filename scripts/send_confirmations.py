7e86b2a04ecd7b5b74611e0e5672da43ca03d015
Hugh O'Reilly <horeilly1101@gmail.com>
2019-10-27 21:27:27 -0500

    update email templates

--- scripts/send_confirmations.py
+++ scripts/send_confirmations.py
@@ -1,5 +1,5 @@
 
-from feather import QuillDao
+from feather import QuillDao, Applicant
 from scripts.constants import Constants
 from feather.email import JinjaEmailFactory, GmailClient
 
@@ -9,6 +9,7 @@ def _main() -> None:
     # retrieve users to email
     dao = QuillDao(Constants.MONGODB_URI, Constants.DB_NAME)
     confirmed_users = dao.get_confirmed_users()
+    # confirmed_users = [Applicant("hjo2@rice.edu", "Hugh", None, None)]
 
     email_factory = JinjaEmailFactory(
         templates_directory_path=Constants.TEMPLATES_PATH,
@@ -18,7 +19,7 @@ def _main() -> None:
     with GmailClient(Constants.EMAIL, Constants.EMAIL_PASSWORD) as client:
         for user in confirmed_users:
             email = email_factory.create_email(
-                email_subject="HackRice 9 Is This Friday!",
+                email_subject="HackRice: A Message from our Sponsors",
                 filename="confirm.html",
                 first_name=user.first_name,
                 to_email=user.email
